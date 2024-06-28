646b54584fea0e3d76f4d5f91de686c245b94801
Benovshe313 <b.mahalova@gmail.com>
2024-06-28 23:23:32 +0400

    File

---
+++ market_ex2/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace market_ex2.Models
+{
+    internal class User
+    {
+    }
+}
---
+++ market_ex2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace market_ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
